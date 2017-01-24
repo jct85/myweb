501730fcdc6beb3a811c1b78e7787333b99ac17f
jct85 <jeancharles.thue@gmail.com>
2017-01-24 17:47:13 +0100

    php

---
+++ cv.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html>
+    <head> <!--L'en tete-->
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="css/style2.css">
+        <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+
+        <title>CV_Thue Jean-CHarles</title> <!--titre de la page-->
+    </head>
+
+    <body> 
+        <Section class ="En-tete"><!--En tete de la page principale -->
+            <div id="banner">    
+                <div id ="banner-left">    
+                </div>
+                <div id ="banner-middle">
+                    <div id="photo">
+                        <a href="index.html"><img src="images/photo-cv.png" alt="Ma Photo" id="photo-jc" /></a>
+                    </div>
+                    <div id="Titre">
+                        <h1> Responsable maintenance </h1>
+                        <h2> Gestionnaire d'équipement  </h2>
+                    </div>
+                </div>
+
+                <div id ="banner-right">
+                    <h3>Contact</h3>
+                    <h8>Jean-Charles Thué</h8> </br>
+                                      </br>
+                    <i class="fa fa-mobile" aria-hidden="true"></i> +33 6 99 88 99 88</br>
+                <i class="fa fa-envelope" aria-hidden="true"></i> jeancharles.thue@gmail.com</br>
+            <i class="fa fa-map-marker" aria-hidden="true"></i> France</br>
+        <i class="fa fa-globe" aria-hidden="true"></i> https://jct85.github.io/
+        </div>
+    </div>
+</Section>
+<Section class ="Corps-cv-part1"><!--Corps du cv -->
+    <div id="Experience">
+        <h4><i class="fa fa-briefcase" aria-hidden="true"></i> Expériences</h4>   
+        <p></p>
+
+        <Section class="Section">
+    
+            <h5>Gestionnaire équipement courant faible/GTB - <span class ="h6">Entreprise Fleury Michon</span><span class="date2"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2014 à aujourd'hui</span></h5>
+             <citation> Production de produits de charcuterie et de jambon de volaille – Groupe : 3715 employés – site de Chantonnay (85) : 320 employés</citation><!--ajouter descriptif entreprise-->
+            <blockquote>
+                <ul>
+                    <li>Référent automatisme/courant faible site </li>
+                    <li>Gestion de projet : 
+                        <ul>
+                            <li>Intégration/migration sytème GTB</li>
+                            <li>Intégration système incendie SSI</li>
+                        </ul>
+                    </li>
+                    <li>Développement système GTB : Création synoptique web, intégration équipements communiquants </li>
+                    <li>Management entreprises exterieures dans les domaines :
+                        <ul>
+                            <li>Electricité</li>
+                            <li>Production/distribution froid</li>
+                            <li>Détection gaz</li>
+                            <li>Détection/extinction incendie</li>
+                            <li>Automatisme</li>
+                        </ul>
+                    </li>
+                    <li>Référent sécurité service maintenance </li>
+                </ul>
+            </blockquote>
+                    <h5>Auto-entrepreneur en informatique - <span class ="h6">Displayinformatique</span> <span class="date1"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2013 à aujourd'hui</span></h5><!--ajouter descriptif entreprise-->
+            <blockquote>
+                <ul>
+                    <li>Installation/configuration réseau informatique PME</li> 
+                    <li>Dépannage informatique pour particulier</li>
+                    <li>Création de site web "vitrine" type CMS (joomla/wordpress)</li>
+                </ul>   
+            </blockquote>
+            <h5>Technicien courant faible/GTB - <span class ="h6">Entreprise FleuryMichon</span><span class="date2"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2008 à 2014</span></h5>
+            <citation> Production de produit de charcuterie et de jambon de volaille – Groupe : 3715 employés – site de Chantonnay (85) : 320 employés</citation>
+            <blockquote>
+               <ul>
+                   <li>Curatif/préventif équipements : 
+                        <ul>
+                            <li>Téléphonie site</li>
+                            <li>Réseau informatique industriel</li>
+                            <li>SSI système détection/extinction incendie</li>
+                            <li>Production froid</li>
+                            <li>Détection Gaz</li>
+                       </ul>
+                    </li>
+                    <li>Formation collaborateur dans les domaines : détection gaz, détéction incendie, utilisation GTB</li>
+                </ul>
+                </blockquote>
+            <h5>Technicien maintenance industriel - <span class ="h6">Entreprise Gastronome</span><span class="date3"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2007 à 2008</span></h5>
+            <citation>Transformation et conservation de viande de volaille – Groupe : 3000 employés – site de Nueil les Aubiers (79) : 199 employés </citation>
+            <blockquote>
+                <ul>
+                    <li>Curatif équipement industriel </li>
+                    <li>Préventif équipement industriel </li>
+                    <li>Rapport intervention sur GMAO </li>
+                </ul>
+            </blockquote>
+        </Section> 
+        
+        <div id="Formations">
+            <h4><i class="fa fa-graduation-cap" aria-hidden="true"></i> Formations</h4>  
+            <p></p>
+            <h5>Formation intégrateur WEB </br>
+            <citation>Cours par correspondance</citation></br>
+            <span class ="h6">Openclassrooms Cité Paradis 75010 Paris
+            </span>
+             
+            <span class="date4"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De Fev 2016 à aujourd'hui
+            </span>
+            </br> 
+           
+            </h5>
+        <h5>Licence M.S.M.S.P Management des services de maintenance, maintenance des </br>    <span class="date5"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2006 à 2007
+        </span> systèmes plurytechniques  </br>
+        <span class ="h6">I.U.T de Saint-Nazaire Rue Michel Ange 44600 Saint-Nazaire
+        </span>
+     
+        </h5>
+    <h5>BTS C.I.R.A Contrôle industriel et régulation automatique </br><span class ="h6">Lycée Saint Jean-Baptispte de la Salle Rue du Ballet 44001 Nantes
+    </span>
+    <span class="date6"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;De 2002 à 2004
+    </span>
+    </h5>
+
+</div>  
+
+</Section>
+<Section class ="Corps-cv-part2"><!--Corps du cv -->
+    <div id="Competence">
+        <h4> <i class="fa fa-puzzle-piece" aria-hidden="true"></i> Compétences</h4>
+        <p></p>
+        <div id="contenu-comptence">
+            <li>
+                <h7>Automatisme industriel :</h7> 
+                <blockquote>
+
+                    <ul>
+                        <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> PL7pro 
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star-half" aria-hidden="true"></i>   
+                        </span></br>
+                    <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Unity 
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star-half" aria-hidden="true"></i> 
+                    </span> </br>
+                <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> VijeoDesigner 
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star-half" aria-hidden="true"></i>
+                </span></br>
+        <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Wincc/setp7
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+        </span> </br>
+    <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Proface 
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star-o" aria-hidden="true"></i>
+    </span>
+    </ul>
+</blockquote>
+<li>
+    <h7>Informatique :</h7> 
+    <blockquote>
+
+        <ul>
+            <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Python 
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star-o" aria-hidden="true"></i>
+                <i class="fa fa-star-o" aria-hidden="true"></i>
+            </span></br>
+        <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Html5 
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+        </span></br>
+    <span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> CSS3 
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star-o" aria-hidden="true"></i>
+    </span></br>
+<span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> Shell 
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+</span></br>
+<span class="programme"><i class="fa fa-puzzle-piece" aria-hidden="true"></i> jQuery
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+</ul>
+</blockquote>
+<li><h7>Gestion de projet</h7>
+  <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+    </li>
+<li><h7>Management</h7>
+  <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i>
+    <i class="fa fa-star-o" aria-hidden="true"></i></li></br>
+<li><h7> Habilitations:
+            <ul>
+                <li>Nacelle</li>
+                <li>Electrique:H2,BR,BC</li>
+                <li>Intervention NH3</li>
+                <li>Intervention risque legionelle.</li>
+                <li>Incendie : E.S.I (équipier de seconde intervention) </li>
+                <li>A.R.I : habilitation au port de l'A.R.I (appareil respiration isolant</li>
+                <li>Intervention Chaufferie: A.Q.I (agent qualifié d'intervention)</li>
+                </ul>
+                </h7>
+            </li>
+
+<li><h7>Langues :</h7></li>
+ <ul>
+     <li><h7>Espagnol : Scolaire</h7></li>
+     <li><h7>Anglais : Professionnel </h7></li>
+</ul>
+</div>
+</div> 
+<Section class="Information-complementaire">
+    <h4> <i class="fa fa-file-text" aria-hidden="true"></i> Informations complémentaires</h4>
+    <p></p>
+    <div id="contenu-complementaire">
+        <ul>
+            <li><h7><i class="fa fa-car" aria-hidden="true"></i> Permis B</h7></li>
+            <li><h7><i class=" fa fa-birthday-cake" aria-hidden="true"></i> 30 ans</h7></li>
+        </ul>
+     
+
+    </div>
+</Section>
+<Section class="Centre d'interet loisir">
+    <h4> <i class="fa fa-futbol-o" aria-hidden="true"></i> Loisirs et centres d’intérêts</h4>
+    <p></p>
+    <div id="contenu-loisir">
+        
+            <ul>
+                
+                <span class="programme"> handball 
+                </span> </br>
+        </br>
+                <span class="programme"> guitare 
+                </span> </br>
+</br>
+                <span class="programme"> Cyclisme 
+                </span> </br>
+</br>
+                <span class="programme"> Electronique : Projet personnel à base de microcontroleur et minipc
+                </span>  </br>
+</br>
+        <span class="programme"> Intérêt pour les nouvelles technologies : domotique
+        </span> 
+    
+        </ul>
+</div>
+</Section>
+</Section>
+
+</Section>
+<div id = "visiteur">
+<?php
+    
+  // Ouverture du fichier "visiteurs.txt"
+$path = "visiteurs.txt"; 
+$file = fopen($path,"r+"); 
+
+// Récupération du nombre de visiteurs 
+$nbvisiteurs = fgets($file,8); 
+if ($nbvisiteurs=="") { 
+   $nbvisiteurs=0; 
+} 
+
+// Enregistrement du nouveau nombre de visiteurs dans "visiteurs.txt" 
+$nbvisiteurs++; 
+fseek($file,0); 
+fputs($file,$nbvisiteurs); 
+
+// Fermeture du fichier "visiteurs.txt"
+fclose($file); 
+
+// Inscription sur la page du nombre de visiteurs 
+echo "Nombre de visiteurs : $nbvisiteurs";   
+    ?>
+
+
+</div>
+</body>
+</html>
